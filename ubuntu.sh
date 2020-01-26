bdedc25c47fc2875150468971fe1f3b816a85b5c
Adrian Griffin <strikersix23@gmail.com>
2020-01-26 15:10:05 -0600

    update to latest nightly
    Updated to ubuntu focal

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,17 +8,17 @@ if [ "$first" != 1 ];then
     if [ ! -f "ubuntu.tar.gz" ]; then
         echo "downloading ubuntu-image"
         if [ "$(dpkg --print-architecture)" = "aarch64" ];then
-            wget http://cdimage.ubuntu.com/ubuntu-base/releases/19.04/release/ubuntu-base-19.04-base-arm64.tar.gz -O ubuntu.tar.gz
+            wget \http://cdimage.ubuntu.com/ubuntu-base/daily/current/focal-base-arm64.tar.gz -O ubuntu.tar.gz
         elif [ "$(dpkg --print-architecture)" = "arm" ];then
-            wget http://cdimage.ubuntu.com/ubuntu-base/releases/19.04/release/ubuntu-base-19.04-base-armhf.tar.gz -O ubuntu.tar.gz
+            wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/focal-base-armhf.tar.gz -O ubuntu.tar.gz
         elif [ "$(dpkg --print-architecture)" = "x86_64" ];then
-            wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/eoan-base-amd64.tar.gz -O ubuntu.tar.gz
+            wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/focal-base-amd64.tar.gz -O ubuntu.tar.gz
         elif [ "$(dpkg --print-architecture)" = "i*86" ];then
-            wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/eoan-base-i386.tar.gz -O ubuntu.tar.gz
+            wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/focal-base-i386.tar.gz -O ubuntu.tar.gz
         elif [ "$(dpkg --print-architecture)" = "x86" ];then
-            wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/eoan-base-i386.tar.gz -O ubuntu.tar.gz
+            wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/focal-base-i386.tar.gz -O ubuntu.tar.gz
         elif [ "$(dpkg --print-architecture)" = "amd64" ];then
-            wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/eoan-base-amd64.tar.gz -O ubuntu.tar.gz
+            wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/focal-base-amd64.tar.gz -O ubuntu.tar.gz
 
 
 
